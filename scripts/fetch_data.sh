4c28f7c63b93f66cc8fbffaa5b8c285282ccf85c
Gregory M. Green <gregorymgreen@gmail.com>
2018-10-15 11:54:16 +0200

    Comments.

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# HI4PI
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd ${script_dir}/../data
 url="https://lambda.gsfc.nasa.gov/data/foregrounds/HI4PI/NHI_HPX.fits"
 echo `pwd`
 wget ${url} 
+
+
+# SFD
+
