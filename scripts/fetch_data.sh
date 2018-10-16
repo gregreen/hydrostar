34c104d1aae3f7941f0c44763144b5da730d8ada
Daniel Lenz <mail@daniellenz.org>
2018-10-16 16:25:27 +0200

    Add SFD to fetch_data.sh

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -2,11 +2,16 @@
 
 # HI4PI
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd ${script_dir}/../data
+cd ${script_dir}/../data/raw
 url="https://lambda.gsfc.nasa.gov/data/foregrounds/HI4PI/NHI_HPX.fits"
 echo `pwd`
 wget ${url} 
 
 
 # SFD
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd ${script_dir}/../data/raw
+url="https://lambda.gsfc.nasa.gov/data/foregrounds/SFD/lambda_sfd_ebv.fits"
+echo `pwd`
+wget ${url} 
 
